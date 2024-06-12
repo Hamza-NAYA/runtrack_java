00eefef9074acc12e9a3628d96a37c7c5ce3c4b3
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-06-12 16:12:27 +0200

    [Terminer] Jour01/Job07

---
+++ Jour01/Job07/Addition.java
@@ -0,0 +1,15 @@
+package Jour01.Job07;
+
+public class Addition {
+    public static void main(String[] args) {
+        // Déclaration de deux variables avec des valeurs 3 et 73
+        int var1 = 3, var2 = 73;
+
+
+        // Effectuer la division de var2 par var1 et stocker le résultat
+        double result = (double) var2 / var1; // Conversion en double pour obtenir un résultat précis
+
+        // Afficher le résultat de la division dans le terminal
+        System.out.println("Le résultat de la division de " + var2 + " par " + var1 + " est : " + result + " meme si la fonction s'appelle Addition");
+    }   
+}
