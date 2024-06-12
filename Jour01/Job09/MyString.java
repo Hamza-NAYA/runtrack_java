46d437dc3593bfeecd926040c1cb7befb16ca213
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-06-12 16:28:45 +0200

    [Terminer] Jour01/Job09

---
+++ Jour01/Job09/MyString.java
@@ -0,0 +1,17 @@
+package Jour01.Job09;
+
+public class MyString {
+    public static void main(String[] args) {
+        // Déclaration et initialisation de variables String de quatre façons différentes
+        String str1 = "String";
+        String str2 = new String("String");
+        String str3 = ""; // Initialisation avec une chaîne vide
+        String str4 = null; // Initialisation avec la valeur null
+
+        // Affichage des valeurs des variables
+        System.out.println("string 1 : " + str1);
+        System.out.println("string 2 : " + str2);
+        System.out.println("string 3 : " + str3);
+        System.out.println("string 4 : " + str4);
+    }
+}
