72520fcb8cc308cbd5485c3a6840cc9f96db42c6
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-06-12 15:24:25 +0200

    [Terminer] Jour01/Job05

---
+++ Jour01/Job05/Calcul.java
@@ -0,0 +1,19 @@
+package Jour01.Job05;
+
+public class Calcul {
+    public static void main(String[] args) {
+        // Déclaration de la variable 'nombre' et assignation de la valeur 7
+        int nombre = 7;
+        
+        // Afficher la valeur initiale de la variable 'nombre'
+        System.out.println("Le nombre est : " + nombre);
+
+        // Ajouter 3 à la variable 'nombre' et afficher la nouvelle valeur
+        nombre += 3;
+        System.out.println("Le nombre plus 3 est : " + nombre);
+
+        // Multiplier par 5 la valeur de 'nombre' et afficher la nouvelle valeur
+        nombre *= 5;
+        System.out.println("Le nombre multiplier par 5 est : " + nombre);
+    }
+}
