cf13a47fccfc8ca3b6d6b70d977ab6bd2f394c61
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-06-12 15:13:41 +0200

    [Terminer] Jour01/Job03

---
+++ Jour01/Job03/HelloWorld.java
@@ -0,0 +1,18 @@
+package Jour01.Job03; // Déclaration du package, assure que la structure des dossiers correspond à celle du package
+
+// Déclaration de la classe publique HelloWorld
+public class HelloWorld {
+
+    // La méthode principale main, point d'entrée de l'application Java
+    public static void main(String[] args) {
+        // Affiche "Hello world" dans le terminal
+        System.out.println("Hello world");
+    }
+    
+    /*
+     * La méthode main est statique car elle doit être appelée
+     * sans avoir besoin d'instancier la classe HelloWorld.
+     * Elle accepte un tableau de chaînes de caractères en argument
+     * qui peut être utilisé pour passer des paramètres en ligne de commande.
+     */
+}
